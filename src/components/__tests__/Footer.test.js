1d821e5af2ef5eabab0e5e382e5542d1b4130f87
Tony W <bobacrypto2@gmail.com>
2023-05-25 10:52:13 -0700

    Create Footer.test.js file

---
+++ src/components/__tests__/Footer.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import Navbar from "../home/Navbar";
+
+describe("Footer component children", () => {
+    it("", () => {
+        
+    })
+})
