d14df45ddd7ccd3e67880b3e03ddc09d74e79741
Tony W <bobacrypto2@gmail.com>
2023-06-07 02:32:35 -0700

    Add test for children of ItemContainer element

--- src/components/__tests__/ItemContainers.test.js
+++ src/components/__tests__/ItemContainers.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
 import ItemContainers, { createItems } from "../shop/ItemContainer";
@@ -14,8 +14,12 @@ describe("ItemContainers component", () => {
     );
     const itemsMock = jest.fn((amount) => amount);
     const elements = createItems(itemsMock(4));
+    const itemContainer = screen.getByRole("region", {
+      name: "item-container",
+    });
 
     expect(elements).toHaveLength(4);
+    expect(itemContainer.childNodes).toHaveLength(12);
   });
 
   it("Our fetch gets called correctly on the Valorant API", async () => {
