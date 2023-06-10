ee24b61d89a534c9960f841058ab5801e4886fab
Tony W <bobacrypto2@gmail.com>
2023-06-09 17:05:12 -0700

    Restore mocks to original implementation after trying alternative method
    Fix hover overlay bug where if the mouse was moving too quickly overlays would not be hidden correctly
    
    Add function to retrieve list of guns / skins based on the default weapon name
    
    Items can now render based on state, and category of items can now be changed based on state as well
    
    Change items to be displayed based on a state variable named page | Test ability to cycle through additional weapons with our new state variable page

--- src/components/shop/ItemContainer.js
+++ src/components/shop/ItemContainer.js
@@ -1,56 +1,69 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/itemContainers.css";
 
 const ItemContainers = () => {
+  const [page, setPage] = useState(1);
+  const [category, setCategory] = useState("vandal");
+  const [catalogue, setCatalogue] = useState({
+    skins: [],
+    page: page,
+  });
+
   async function populateDisplay() {
-    const vandals = [
-      "k/tac",
-      "prime",
-      "elderflame",
-      "chronovoid",
-      "ion",
-      "gaiasvengeance",
-      "reaver",
-      "sentinelsoflight",
-      "neptune",
-      "rgx11zpro",
-      "preludetochaos",
-      "oni",
-    ];
-
-    const weapons = await fetchWeapons();
-
-    const skinsArr = weapons.data[2].skins;
-    const foundVandal = [];
-
     const images = document.querySelectorAll("[data-testid='shop-page'] img");
-
-    for (let i = 0; i < vandals.length; i++) {
-      for (let j = 0; j < skinsArr.length; j++) {
-        const skinName = skinsArr[j].displayName
-          .replace(/\s|[']|[.]|Vandal/g, "")
-          .toLowerCase();
-        if (vandals.includes(skinName) && !foundVandal.includes(skinsArr[j])) {
-          foundVandal.push(skinsArr[j]);
+    if (catalogue.skins.length > 0) {
+      for (let k = 0; k < images.length; k++) {
+        if (catalogue.skins[k].chromas) {
+          images[k].src = `${catalogue.skins[k].chromas[0].fullRender}`;
+        } else {
+          images[k].src = `${catalogue.skins[k].displayIcon}`;
         }
-      }
-    }
-
-    for (let k = 0; k < images.length; k++) {
-      if (foundVandal[k].fullRender) {
-        images[k].src = `${foundVandal[k].chromas[1].fullRender}`;
-      } else {
-        images[k].src = `${foundVandal[k].displayIcon}`;
       }
     }
   }
 
-  function handleHover(element) {
-    const options = element.target.querySelector("#item-options");
-    if (options) {
-      options.classList.toggle("shown");
-      options.classList.toggle("hidden");
+  async function catalogueState() {
+    const skins = await getItems(category, page);
+
+    setCatalogue({ skins: skins, page: page });
+  }
+
+  async function getItems(name, page) {
+    const weapons = await fetchWeapons();
+    let end = page * 12;
+    let skins = [];
+
+    for (let item of weapons.data) {
+      if (item.displayName.toLowerCase() === name.toLowerCase()) {
+        for (let i = end - 12; i < end; i++) {
+          skins.push(item.skins[i]);
+        }
+        return skins;
+      }
     }
+  }
+
+  function handleHover(e) {
+    const options = document.querySelectorAll(".item-options");
+    const target = e.target.querySelector(".item-options");
+
+    if (target) {
+      target.classList.toggle("shown");
+      target.classList.toggle("hidden");
+    }
+
+    for (let element of options) {
+      if (element !== target && element.classList.contains("shown")) {
+        element.classList.toggle("shown");
+        element.classList.toggle("hidden");
+      }
+    }
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    setPage(() => page + 1);
+    // setCategory("phantom");
   }
 
   function createItems(amount) {
@@ -60,13 +73,15 @@ const ItemContainers = () => {
       const item = (
         <div
           key={i}
-          className="flex min-h-full w-64 min-w-fit items-center justify-center border-2 border-solid border-gray-900"
+          className="item flex min-h-full w-64 min-w-fit items-center justify-center border-2 border-solid border-gray-900"
           onMouseEnter={(e) => handleHover(e)}
           onMouseLeave={(e) => handleHover(e)}
         >
           <img src="" alt="" id={i} className="w-9/12"></img>
-          <div data-testid="item-options" id="item-options" className="hidden">
-            <button aria-label="add-to-cart">Add to Cart</button>
+          <div data-testid="item-options" className="item-options hidden">
+            <button aria-label="add-to-cart" onClick={handleClick}>
+              Add to Cart
+            </button>
           </div>
         </div>
       );
@@ -78,7 +93,11 @@ const ItemContainers = () => {
 
   useEffect(() => {
     populateDisplay();
-  }, []);
+  }, [catalogue]);
+
+  useEffect(() => {
+    catalogueState();
+  }, [category, page]);
 
   return (
     <section
