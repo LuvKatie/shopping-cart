91f3c996b68f8222cfee2733e4465287350b59b0
Tony W <bobacrypto2@gmail.com>
2023-06-12 04:40:03 -0700

    Migrate code within ItemContainers.js to a new Items.js component | Migrate tests meant for Items.test.js from ItemContainers.test.js
    Add feature to only display the amount of items left over rather than the default of the maximum our ItemContainer element can fit
    
    Rewrite tests to render items correctly for assertions

--- src/components/__tests__/ItemContainers.test.js
+++ src/components/__tests__/ItemContainers.test.js
@@ -1,71 +1,28 @@
 import React from "react";
 import { act } from "react-test-renderer";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
 import ItemContainers from "../shop/ItemContainer";
-import { fetchWeapons } from "../shop/ItemContainer";
 
-const WEAPONS = {
-  data: [
-    { displayName: "ares" },
-    { displayName: "phantom" },
-    { displayName: "vandal", skins: [] },
-  ],
-};
-
-beforeEach(() => {
-  act(() => {
+beforeEach(async () => {
+  await act(async () => {
     render(
       <MemoryRouter>
         <ItemContainers />
       </MemoryRouter>
     );
   });
-  //eslint-disable-next-line
-  fetchMock = jest.spyOn(global, "fetch").mockImplementation(() =>
-    Promise.resolve({
-      json: () => Promise.resolve(WEAPONS),
-    })
-  );
-});
-
-afterEach(() => {
-  jest.restoreAllMocks();
 });
 
 describe("ItemContainers component", () => {
-  it("ItemContainer component renders correct amount of children elements", () => {
+  it("ItemContainer component renders correct amount of children elements", async () => {
     const itemContainer = screen.getByRole("region", {
       name: "item-container",
     });
 
-    expect(itemContainer.childNodes).toHaveLength(12);
-  });
-
-  it("Our fetch gets called correctly on the Valorant API", async () => {
-    const response = await fetchWeapons();
-
-    //eslint-disable-next-line
-    expect(fetchMock).toHaveBeenCalled();
-    //eslint-disable-next-line
-    expect(fetchMock).toHaveBeenCalledWith(
-      "https://valorant-api.com/v1/weapons",
-      { mode: "cors" }
-    );
-    expect(response.data[2].displayName).toEqual("vandal");
-  });
-
-  it("Hovering over an item will show 'Add to cart' button", async () => {
-    const user = userEvent.setup();
-
-    const item = screen.getByRole("region", {
-      name: "item-container",
-    }).firstChild;
-
-    await user.hover(item);
-    const button = screen.getAllByText("Add to Cart");
-    expect(button[0]).toBeInTheDocument();
+    await waitFor(() => {
+      expect(itemContainer.childNodes).toHaveLength(12);
+    });
   });
 });
--- src/components/shop/ItemContainer.js
+++ src/components/shop/ItemContainer.js
@@ -1,121 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../../styles/itemContainers.css";
+import Items from "./Items";
 
 const ItemContainers = () => {
-  const [page, setPage] = useState(1);
   const [category, setCategory] = useState("vandal");
-  const [catalogue, setCatalogue] = useState({
-    skins: [],
-    page: page,
-  });
-
-  async function populateDisplay() {
-    const images = document.querySelectorAll("[data-testid='shop-page'] img");
-    if (catalogue.skins.length > 0) {
-      for (let k = 0; k < images.length; k++) {
-        if (catalogue.skins[k].chromas) {
-          images[k].src = `${catalogue.skins[k].chromas[0].fullRender}`;
-        } else {
-          images[k].src = `${catalogue.skins[k].displayIcon}`;
-        }
-      }
-    }
-  }
-
-  async function catalogueState() {
-    const skins = await getItems(category, page);
-
-    setCatalogue({ skins: skins, page: page });
-  }
-
-  async function getItems(name, page) {
-    const weapons = await fetchWeapons();
-    let end = page * 12;
-    let skins = [];
-
-    for (let item of weapons.data) {
-      if (item.displayName.toLowerCase() === name.toLowerCase()) {
-        for (let i = end - 12; i < end; i++) {
-          skins.push(item.skins[i]);
-        }
-        return skins;
-      }
-    }
-  }
-
-  function handleHover(e) {
-    const options = document.querySelectorAll(".item-options");
-    const target = e.target.querySelector(".item-options");
-
-    if (target) {
-      target.classList.toggle("shown");
-      target.classList.toggle("hidden");
-    }
-
-    for (let element of options) {
-      if (element !== target && element.classList.contains("shown")) {
-        element.classList.toggle("shown");
-        element.classList.toggle("hidden");
-      }
-    }
-  }
-
-  function handleClick(e) {
-    e.preventDefault();
-    setPage(() => page + 1);
-    // setCategory("phantom");
-  }
-
-  function createItems(amount) {
-    const elements = [];
-
-    for (let i = 0; i < amount; i++) {
-      const item = (
-        <div
-          key={i}
-          className="item flex min-h-full w-64 min-w-fit items-center justify-center border-2 border-solid border-gray-900"
-          onMouseEnter={(e) => handleHover(e)}
-          onMouseLeave={(e) => handleHover(e)}
-        >
-          <img src="" alt="" id={i} className="w-9/12"></img>
-          <div data-testid="item-options" className="item-options hidden">
-            <button aria-label="add-to-cart" onClick={handleClick}>
-              Add to Cart
-            </button>
-          </div>
-        </div>
-      );
-      elements.push(item);
-    }
-
-    return elements;
-  }
-
-  useEffect(() => {
-    populateDisplay();
-  }, [catalogue]);
-
-  useEffect(() => {
-    catalogueState();
-  }, [category, page]);
 
   return (
     <section
       aria-label="item-container"
       className="item-layout grid gap-12 bg-white p-6"
     >
-      {createItems(12)}
+      <Items category={category} />
     </section>
   );
 };
-
-export async function fetchWeapons() {
-  const response = await fetch("https://valorant-api.com/v1/weapons", {
-    mode: "cors",
-  });
-  const weapons = await response.json();
-
-  return weapons;
-}
 
 export default ItemContainers;
